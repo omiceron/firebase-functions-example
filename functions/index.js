c8fee78368f8c55aa50b87e7be5dcfb761dec7e7
Constantine Sharow <omiceron@gmail.com>
2019-01-19 16:49:28 +0500

    SendMessage Updated

--- functions/index.js
+++ functions/index.js
@@ -4,12 +4,23 @@ admin.initializeApp()
 
 exports.sendMessage = functions.https.onCall((data, context) => {
   const text = data.text
+  const chatId = data.chatId
+  const userId = context.auth.uid
 
-  if (!text) return 'notext'
+  if (!text || !chatId) return 'notext'
 
-  admin.database().ref('/helloWorld').push({
-    text: text
-  })
+  const ref = admin.database()
+    .ref('chats')
+    .child(chatId)
+    .child('messages')
+
+  const message = {
+    text: text,
+    user: userId,
+    timestamp: admin.database.ServerValue.TIMESTAMP
+  }
+
+  ref.push(message)
 
   return text
 })
