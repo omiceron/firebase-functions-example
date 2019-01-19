da8e8cf3e93cd949170e65b187f5ff0eec4c1584
Constantine Sharow <omiceron@gmail.com>
2019-01-19 22:23:23 +0500

    Update sendMessage: now all users get message

--- functions/index.js
+++ functions/index.js
@@ -2,17 +2,16 @@ const functions = require('firebase-functions')
 const admin = require('firebase-admin')
 admin.initializeApp()
 
+const getChatReference = require('./utils').getChatReference
+const updateChat = require('./utils').updateChat
+const getUserChatsReference = require('./utils').getUserChatsReference
+
 exports.sendMessage = functions.https.onCall((data, context) => {
   const text = data.text
   const chatId = data.chatId
   const userId = context.auth.uid
 
-  if (!text || !chatId) return 'notext'
-
-  const ref = admin.database()
-    .ref('chats')
-    .child(chatId)
-    .child('messages')
+  if (!text || !chatId) throw new Error('Message text or chatId error')
 
   const message = {
     text: text,
@@ -20,7 +19,25 @@ exports.sendMessage = functions.https.onCall((data, context) => {
     timestamp: admin.database.ServerValue.TIMESTAMP
   }
 
-  ref.push(message)
+  const chatReference = getChatReference(chatId)
+  const senderReference = getUserChatsReference(userId)
+
+  updateChat(senderReference, message, chatId)
+
+  senderReference
+    .orderByChild('chatId')
+    .equalTo(chatId)
+    .once('value')
+    .then(snapshot => {
+      const data = snapshot.val()
+      const key = Object.keys(data)[0]
+      const chat = data[key]
+      const recipientReference = getUserChatsReference(chat.userId)
+
+      return updateChat(recipientReference, message, chatId)
+    })
+
+  chatReference.push(message)
 
   return text
 })
