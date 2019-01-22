7d8462a477730266e9968cbeb28a96ab6bd4a930
Constantine Sharow <omiceron@gmail.com>
2019-01-22 15:27:21 +0500

    Add asynchronous functional

--- functions/index.js
+++ functions/index.js
@@ -1,41 +1,38 @@
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
+const {
+  getChatReference,
+  updateHelper,
+  getUserChatsReference,
+} = require('./utils')
+
 admin.initializeApp()
 
-const getChatReference = require('./utils').getChatReference
-const updateChat = require('./utils').updateChat
-const getUserChatsReference = require('./utils').getUserChatsReference
-
-exports.sendMessage = functions.https.onCall((data, context) => {
-  const text = data.text
-  const chatId = data.chatId
-  const userId = context.auth.uid
+exports.sendMessage = functions.https.onCall(async (data, context) => {
+  const {text, chatId} = data
+  const user = context.auth.uid
 
   if (!text || !chatId) throw new Error('Message text or chatId error')
 
   const message = {
-    text: text,
-    user: userId,
-    timestamp: admin.database.ServerValue.TIMESTAMP
+    text,
+    user,
+    timestamp: admin.database.ServerValue.TIMESTAMP,
   }
 
   const chatReference = getChatReference(chatId)
-  const senderReference = getUserChatsReference(userId)
+  const senderReference = getUserChatsReference(user)
 
-  updateChat(senderReference, message, chatId)
+  const updateReference = async ({key, message, ref}) =>
+    ref.child(key)
+      .child('lastMessage')
+      .update(message)
 
-  senderReference
-    .orderByChild('chatId')
-    .equalTo(chatId)
-    .once('value')
-    .then(snapshot => {
-      const data = snapshot.val()
-      const key = Object.keys(data)[0]
-      const chat = data[key]
-      const recipientReference = getUserChatsReference(chat.userId)
+  const getAndUpdateReference = async ({chat: {userId}, message}) =>
+    updateHelper(getUserChatsReference(userId), message, chatId, updateReference)
 
-      return updateChat(recipientReference, message, chatId)
-    })
+  updateHelper(senderReference, message, chatId, updateReference)
+  updateHelper(senderReference, message, chatId, getAndUpdateReference)
 
   chatReference.push(message)
 
--- functions/utils.js
+++ functions/utils.js
@@ -1,25 +1,32 @@
 const admin = require('firebase-admin')
 
-exports.getUserChatsReference = (userId) => {
-  return admin.database()
+const getUserChatsReference = (userId) =>
+  admin
+    .database()
     .ref('people')
     .child(userId)
     .child('chats')
-}
 
-exports.getChatReference = (chatId) => admin.database()
-  .ref('chats')
-  .child(chatId)
-  .child('messages')
+const getChatReference = (chatId) =>
+  admin
+    .database()
+    .ref('chats')
+    .child(chatId)
+    .child('messages')
 
-exports.updateChat = (ref, message, chatId) =>
-  ref.orderByChild('chatId')
+const updateHelper = async (ref, message, chatId, callback) =>
+  ref
+    .orderByChild('chatId')
     .equalTo(chatId)
     .once('value')
     .then(snapshot => {
-      const key = Object.keys(snapshot.val())[0]
+      const data = snapshot.val()
+      const [key, chat] = Object.entries(data)[0]
 
-      return ref.child(key)
-        .child('lastMessage')
-        .update(message)
+      return callback({key, chat, message, ref})
     })
+
+
+exports.getUserChatsReference = getUserChatsReference
+exports.getChatReference = getChatReference
+exports.updateHelper = updateHelper
